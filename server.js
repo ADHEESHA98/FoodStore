e898e6d3a5d09ef5be5ddf14e45cfc46d83bb980
ADHEESHA98 <chathumalwanigasekara@gmail.com>
2022-10-11 23:05:39 +0530

    coding server.js file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path")
+
+dotenv.config();
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB was connected Successfully");
+});
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8070;
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running port ${PORT}`);
+});
